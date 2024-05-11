eb9059afc5b7e35e05d58622be37682751d71807
Rudradeep Guha <rudradeep4@gmail.com>
2024-05-11 17:45:12 +0200

    change api calls

--- pages/api/trials.js
+++ pages/api/trials.js
@@ -1,33 +1,75 @@
-import nextConnect from 'next-connect';
-// import middleware from '../../middleware/database';
+// import nextConnect from 'next-connect';
+// // import middleware from '../../middleware/database';
 
-const handler = nextConnect()
+// const handler = nextConnect()
 
-handler.use(middleware)
+// handler.use(middleware)
 
-// handler.get(async (req, res) => {
-//     let doc = await req.db.collection('trials').find().toArray()
-//     res.json(doc)
+// // handler.get(async (req, res) => {
+// //     let doc = await req.db.collection('trials').find().toArray()
+// //     res.json(doc)
+// // })
+
+// handler.post(async (req, res) => {
+//     let data = req.body;
+//     data = JSON.parse(data);
+   
+//     const filter = { video_path: data.trialId };
+//     const updateDoc = {$push:
+//         {responses: {
+//             "subject": data.subject,
+//             "age": data.age,
+//             "sex": data.sex,
+//             "order": data.order,
+//             "trial_num": data.count,
+//             "response": data.response
+//         }}
+//     };
+
+//     let result = await req.db.collection('trials').updateOne(filter, updateDoc)
+//     res.json({message: 'ok'});
 // })
 
-handler.post(async (req, res) => {
-    let data = req.body;
-    data = JSON.parse(data);
-   
-    const filter = { video_path: data.trialId };
-    const updateDoc = {$push:
-        {responses: {
-            "subject": data.subject,
-            "age": data.age,
-            "sex": data.sex,
-            "order": data.order,
-            "trial_num": data.count,
-            "response": data.response
-        }}
-    };
+// export default handler
 
-    let result = await req.db.collection('trials').updateOne(filter, updateDoc)
-    res.json({message: 'ok'});
-})
+import { MongoClient } from "mongodb"
 
-export default handler
+export default async function handler(req, res) {
+    switch (req.method) {
+        case 'POST': {
+            return addSong(req, res);
+        }
+    }
+}
+
+async function addSong(req, res) {
+
+    try {
+        // connect to the database
+        const client = await MongoClient.connect(process.env.MONGODB_URI)
+        const db = client.db('CLuster0')
+
+        // update with response
+        const filter = { video_path: data.trialId };
+        const updateDoc = {$push:
+            {responses: {
+                "subject": data.subject,
+                "age": data.age,
+                "sex": data.sex,
+                "order": data.order,
+                "trial_num": data.count,
+                "response": data.response
+            }}
+        };
+
+        await req.db.collection('trials').updateOne(filter, updateDoc)
+        return res.json({
+            message: 'ok'
+        });
+    } catch (error) {
+        return res.json({
+            message: new Error(error).message,
+            success: false,
+        })
+    }
+}
